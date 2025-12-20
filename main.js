921988ec127e76af6b61b1dcd90499f9d365b248
Gasser Zayed <gasserzayed.work0@gmail.com>
2025-12-20 15:58:22 +0200

    Refactor nav menu toggle functionality
    Simplified the toggle functionality for the navigation menu.

--- main.js
+++ main.js
@@ -3,12 +3,5 @@ const navMenu = document.getElementById("navMenu");
 
 toggleBtn.addEventListener("click", () => {
   navMenu.classList.toggle("hidden");
-  navMenu.classList.toggle("flex");
-  navMenu.classList.toggle("flex-col");
-  navMenu.classList.toggle("absolute");
-  navMenu.classList.toggle("top-16");
-  navMenu.classList.toggle("right-4");
-  navMenu.classList.toggle("bg-orange-300");
-  navMenu.classList.toggle("p-4");
-  navMenu.classList.toggle("rounded-xl");
 });
+
